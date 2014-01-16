ad1f0cde21d5d90fff8c31014cdd9b5a20a10812
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-01-16 18:31:10 +0000

    Main view added

---
+++ PhoneboxLogger/MCMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCMapViewController.h
+//  PhoneboxLogger
+//
+//  Created by Matthew Cheetham on 16/01/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCMapViewController : UIViewController
+
+@end
---
+++ PhoneboxLogger/MCMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MCMapViewController.m
+//  PhoneboxLogger
+//
+//  Created by Matthew Cheetham on 16/01/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import "MCMapViewController.h"
+
+@interface MCMapViewController ()
+
+@end
+
+@implementation MCMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
