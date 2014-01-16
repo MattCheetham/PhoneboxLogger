f5ed114bcd4b0eded7d5c252683e48c11bf18b6c
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-01-16 18:26:05 +0000

    Initial commit
    Created project

---
+++ PhoneboxLogger/MCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MCAppDelegate.h
+//  PhoneboxLogger
+//
+//  Created by Matthew Cheetham on 16/01/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhoneboxLogger/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhoneboxLogger
+//
+//  Created by Matthew Cheetham on 16/01/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MCAppDelegate class]));
+    }
+}
