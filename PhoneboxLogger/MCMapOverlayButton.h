6922bd6449fd30461eb2089c239f860add340d6a
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-01-16 18:49:32 +0000

    Created button

---
+++ PhoneboxLogger/MCMapOverlayButton.h
@@ -0,0 +1,13 @@
+//
+//  MCMapOverlayButton.h
+//  PhoneboxLogger
+//
+//  Created by Matthew Cheetham on 16/01/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCMapOverlayButton : UIButton
+
+@end
---
+++ PhoneboxLogger/MCMapOverlayButton.m
@@ -0,0 +1,34 @@
+//
+//  MCMapOverlayButton.m
+//  PhoneboxLogger
+//
+//  Created by Matthew Cheetham on 16/01/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import "MCMapOverlayButton.h"
+
+@implementation MCMapOverlayButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        self.titleLabel.font = [UIFont boldSystemFontOfSize:14];
+        
+        self.layer.cornerRadius = 17;
+        self.layer.borderWidth = 1.0;
+        self.alpha = 0.8;
+        self.showsTouchWhenHighlighted = YES;
+        
+        [self setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        self.backgroundColor = [UIColor colorWithWhite:1 alpha:0.8];
+        self.layer.borderColor = [UIColor darkGrayColor].CGColor;
+        
+    }
+    return self;
+}
+
+
+@end
